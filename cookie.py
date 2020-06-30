e766ee85096ffd1a8e1a4bff043521867ab8dae8
室伏 美緒 <mio.murofushi@d2c.co.jp>
2020-06-30 18:55:40 +0900

    Add:cookie

--- cookie.py
+++ cookie.py
@@ -6,47 +6,36 @@ import datetime
 app = Flask(__name__)
 app.secret_key = 'hogehoge'
 
-lastlogin=""
-nowlogin=""
-
-
 @app.route("/cookie", methods=["POST","GET"])
 def sample_cookie():
     first_mes=""
     re_mes=""
+    lastlogin=""
+    nowlogin=""
+    mes = ""
 
     # リクエストを送信する回数をカウント
     # リクエストを送信した時間を受け取る
     count = request.cookies.get('count')
-    lastlogin = request.cookies.get("lastlogin", "")
+    lastlogin = request.cookies.get("nowlogin", "")
     nowlogin = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
-    # カウント確認
-    if count is None:
-        count = 1
-        first_mes = "初めてのアクセスです。"
-        print(first_mes)
-    else:
-        count = int(count) + 1
-        re_mes = "カウントを追加しました！"
-        print(re_mes)
-    
-    response = make_response()
-
+    # delボタンが押されたかどうかを判断
     if "del" in request.form.keys():
-        response.set_cookie('count', str(count), expires=0)
-        response.set_cookie('lastlogin', lastlogin, expires=0)
-        response.set_cookie('nowlogin', nowlogin, expires=0)
-        print("cookie削除しました")
-
+        mes = "削除ボタンが押されました"
     else:
-        # count 
-        # last_access 日時情報
-        response.set_cookie('count', str(count))
-        response.set_cookie('lastlogin', lastlogin)
-        response.set_cookie('nowlogin', nowlogin)
-    
+        # カウント確認
+        if count is None:
+            count = 1
+            first_mes = "初めてのアクセスです。"
+            print(first_mes)
+        else:
+            count = int(count) + 1
+            re_mes = "カウントを追加しました！"
+            print(re_mes)
+
     params = {
+        "mes":mes,
         "first_mes":first_mes,
         "re_mes":re_mes,
         "count":count,
@@ -54,7 +43,21 @@ def sample_cookie():
         "nowlogin":nowlogin
     }
     
-    return render_template("cookie.html", **params)
+    response = make_response(render_template("cookie.html", **params))
+
+    if "del" in request.form.keys():
+        response.set_cookie('count', str(count), expires=0)
+        response.set_cookie('lastlogin', lastlogin, expires=0)
+        response.set_cookie('nowlogin', nowlogin, expires=0)
+        print("cookie削除")
+
+    else:
+        response.set_cookie('count', str(count))
+        response.set_cookie('lastlogin', lastlogin)
+        response.set_cookie('nowlogin', nowlogin)
+    
+    #return render_template("cookie.html", **params)
+    return response
 
 @app.route("/session")
 def sample_session():
